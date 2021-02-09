d35d665d48d6075d3237056a053b5f95672449ed
janig7 <karoljanik17@gmail.com>
2021-02-09 20:09:54 +0100

    created logic for game

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
-const polakChuj = 'polak chuj';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-const calcAge = birthYear => 2037 - birthYear;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
